4682dea94b1c7b640d98eddd9b2e0712a16be663
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-10-03 21:12:01 -0500

    Update minEatingSpeed.py

--- minEatingSpeed.py
+++ minEatingSpeed.py
@@ -1,2 +1,9 @@
 
 # 875. Koko Eating Bananas
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
